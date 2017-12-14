c59b2488a87ba2d6f07b6629e9c885f3f497920a
Vladimir Smikovsky <vladimireur@gmail.com>
2017-12-14 20:02:24 +0200

    start write

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const TelegramlBot = recuire('node-telegram-bot-api'),                                ')
+const TelegramlBot = recuire('node-telegram-bot-api'),        
 	  request = require('request'),
 	  //нативный модуль ноды работающий с файлами на сервере
 	  fs = require('fs'),
-	  token = '',
+	  // Bot: CapBoss_bot and my__bot_telegram
+	  token = '468088216:AAGH2J2b0wpnuCc9Y3k7wiV1iZsisPXnd3c',
 	  bot = new TelegramlBot(token, {polling: true});		
 
 
