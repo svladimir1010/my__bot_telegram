36f70bcbfd1719649f31cf0d7e2935f3d6597076
Vladimir Smikovsky <vladimireur@gmail.com>
2017-12-14 18:56:21 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const TelegramlBot = recuire('node-telegram-bot-api'),                                ')
+	  request = require('request'),
+	  //нативный модуль ноды работающий с файлами на сервере
+	  fs = require('fs'),
+	  token = '',
+	  bot = new TelegramlBot(token, {polling: true});		
+
+
+
+
+
